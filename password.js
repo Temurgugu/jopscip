81f7de60e1bf5cc01f244a2893e8ed51b081d71e
Temurgugu <t.gugushvili@hotmail.com>
2025-08-23 18:30:30 +0400

    update

--- password.js
+++ password.js
@@ -1,7 +1,7 @@
 <script>
 document.addEventListener("DOMContentLoaded", function() {
   var pass = prompt("Enter password to access JOPSCIP Workshop site:");
-  if (pass !== "yourpassword123") {
+  if (pass !== "JOPSCIP") {
     document.body.innerHTML = "<h2 style='text-align:center;margin-top:50px;'>Access Denied</h2>";
   }
 });
